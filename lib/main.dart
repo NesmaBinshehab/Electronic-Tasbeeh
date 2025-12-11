94d2015e5bc10884a2ebab0a191eb0dc2c73bad2
unknown <nesmabinshehab@gmail.com>
2025-12-11 08:42:05 -0800

    textfeild and send data between pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/secondpage.dart';
 
-import 'aboutpage.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -14,47 +12,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Named Routes Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeScreen(),
-        '/SecondPage': (context) => const SecondPage(),
-        '/about': (context) => const AboutScreen(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Homepage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  TextEditingController _controller1 = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
+
+  void _login() {
+    setState(() {
+      _controller2.text = _controller1.text;
+    });
+  }
+
+  void _Nav2() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Secandpage(
+              name: _controller1.text,
+            )));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async{
-                final result = await Navigator.pushNamed(
-                    context,'/SecondPage'
-                )as bool?;
-                if (result != null) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(result?'You selected accept':'you selected not to accept')),
-                  );
-                }
-              },
-              child: const Text('Go to selection screen'),
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 5,
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+
+                /// TextField 1
+                TextField(
+                  controller: _controller1,
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                    hintText: 'Nesma',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                /// TextField 2 (readOnly)
+                TextField(
+                  controller: _controller2,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: 'Name output',
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                /// Buttons
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: _login, child: const Text("Submit")),
+                ),
+                const SizedBox(height: 15),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: _Nav2, child: const Text('Next Page')),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/about');
-              },
-              child: const Text('Go to About'),
-            ),
-          ],
+          ),
         ),
       ),
     );
